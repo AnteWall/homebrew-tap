753f7cd022a776e85ee98d20561edc2fdceb102d
Ante Wall <antewall@gmail.com>
2026-09-26 15:47:05 +0000

    Update avanza-tools to 1.0.2

--- Formula/avanza-tools.rb
+++ Formula/avanza-tools.rb
@@ -1,8 +1,8 @@
 class AvanzaTools < Formula
   desc "Command-line interface for Avanza"
   homepage "https://github.com/AnteWall/avanza-ts"
-  url "https://registry.npmjs.org/avanza-tools/-/avanza-tools-1.0.0.tgz"
-  sha256 "66e1f9438e8487f10024c7a1fb4a377d6a2f22a666a0b37d7920534fd599ee32"
+  url "https://github.com/AnteWall/avanza-ts/releases/download/avanza-tools@1.0.2/avanza-tools-1.0.2.tgz"
+  sha256 "571c92d4866cb50c58b273659a5f8d8c4df8cffa04c49b0bdbe046459f8a0b66"
 
   depends_on "node"
 
